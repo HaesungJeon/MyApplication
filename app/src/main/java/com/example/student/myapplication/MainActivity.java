3c638e660a26a236f5c690208afa98cfabe9e40f
student <eternalasset@ajou.ac.kr>
2018-04-10 19:01:16 +0900

    ////////변경용 달음

--- app/src/main/java/com/example/student/myapplication/MainActivity.java
+++ app/src/main/java/com/example/student/myapplication/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         System.out.println("modified");
+        System.out.println("modified again");
     ////////////////////////////////////////
     }
 }
